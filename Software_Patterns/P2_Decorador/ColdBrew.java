5f6d53c52dcd3d83c4c646434e48a9ec64a89292
jorgexe <jorgexe.sandoval@gmail.com>
2024-02-26 15:42:57 -0800

    add P2

---
+++ Software_Patterns/P2_Decorador/ColdBrew.java
@@ -0,0 +1,11 @@
+// Nueva bebida ColdBrew
+public class ColdBrew extends Beverage{
+    
+    public ColdBrew() {
+        description = "Cold Brew";
+    }
+    
+    public double cost() {
+        return 3.99;
+    }
+}
---
+++ Software_Patterns/P2_Decorador/HotChocolate.java
@@ -0,0 +1,10 @@
+// Nueva bebida HotChocolate
+public class  HotChocolate extends Beverage{
+	public HotChocolate() {
+		description = "Hot Chocolate";
+	}
+  
+	public double cost() {
+		return 1.49;
+	} 
+}
